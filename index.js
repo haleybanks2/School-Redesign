d3d6def1b6615d09b10e77661ec98928f442d256
haleybanks2 <hbanks0025@kctcs.edu>
2017-03-31 13:24:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+$("#link1").mouseenter(function(){
+    $("#link1").css("font-size", "30px");
+});
+$("#link1").mouseleave(function(){
+    $("#link1").css("font-size", "25px");
+});
+$("#link2").mouseenter(function(){
+    $("#link2").css("font-size", "30px");
+});
+$("#link2").mouseleave(function(){
+    $("#link2").css("font-size", "25px");
+});
+$("#link3").mouseenter(function(){
+    $("#link3").css("font-size", "30px");
+});
+$("#link3").mouseleave(function(){
+    $("#link3").css("font-size", "25px");
+});
